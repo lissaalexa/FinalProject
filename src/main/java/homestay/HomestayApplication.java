13af4dc2eda7fe2caedfa420d530c734193b1452
lissaalexa <127424892+lissaalexa@users.noreply.github.com>
2023-06-30 22:19:34 -0400

    Update June 30th

--- src/main/java/homestay/HomestayApplication.java
+++ src/main/java/homestay/HomestayApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@SpringBootApplication
-//@EnableJpaRepositories(basePackages = "pet.store")
-//@EntityScan(basePackages = "pet.store.entity")
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "homestay")
+@EntityScan(basePackages = "homestay.entity")
 public class HomestayApplication {
 	public static void main(String[] args) {
-		//SpringApplication.run(PetStoreApplication.class, args);
+		SpringApplication.run(HomestayApplication.class, args);
 	}
 }
