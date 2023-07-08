3b58deb6837ea5a6137ca4a85af74c981f44e775
lissaalexa <127424892+lissaalexa@users.noreply.github.com>
2023-07-07 23:56:08 -0400

    Update July 7th

--- src/main/java/homestay/controller/model/StudentData.java
+++ src/main/java/homestay/controller/model/StudentData.java
@@ -1,7 +1,10 @@
 package homestay.controller.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import homestay.entity.Preference;
 import homestay.entity.Student;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,7 @@ public class StudentData {
     private String studentLastName;		//last name of the student
     private LocalDate studentDateOfBirth;	//date of birth of the student
 	private String studentCountry;		//the country the student is from
+	private Set<PreferenceData> preferences = new HashSet<>();	//set of preferences associated with the student
 	
     public StudentData(Student student) {
     	studentId = student.getStudentId();		//set the student ID from the provided Student entity
@@ -21,5 +25,9 @@ public class StudentData {
 	    studentLastName = student.getStudentLastName();		//set the student's last name from the provided Student entity
 	    studentDateOfBirth = student.getStudentDateOfBirth();	//set the student's date of birth from the provided Student entity
 	    studentCountry = student.getStudentCountry();		//set the student's country from the provided Student entity
+	    
+	    for (Preference preference : student.getPreferences()) {	//set the preferences
+			preferences.add(new PreferenceData(preference) );
+		}
     }
 }
